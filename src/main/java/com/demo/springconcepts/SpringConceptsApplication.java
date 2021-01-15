1929bb0370bd145fac3d2933a22ecf830df857c6
MURA199918 <muralisaranr18@gmail.com>
2021-01-15 09:42:36 +0530

    [Murali]Refactored to display welcome message

--- src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringConceptsApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Spring Concepts Demo");
 		SpringApplication.run(SpringConceptsApplication.class, args);
 	}
 
